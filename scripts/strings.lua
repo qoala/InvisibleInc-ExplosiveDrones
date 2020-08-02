0857b4fd09803d99d4867cfa8397e59deb6ba573
Qoalabear <512290+qoala@users.noreply.github.com>
2020-08-01 22:05:20 -1000

    Shorten the ability description to fit in 3 lines

--- scripts/strings.lua
+++ scripts/strings.lua
@@ -3,7 +3,7 @@ local DLC_STRINGS =
 {
 	ABILITIES = {
 		EXPLOSIVEDRONE = "EXPLOSIVE DRONE",
-		EXPLOSIVEDRONE_DESC = "Detonates when an active enemy is on the same tile.\nIgnores cloak.\nLethal damage.\nEmits a loud sound with a range of 8 tiles",
+		EXPLOSIVEDRONE_DESC = "Detonates when an active enemy is on the same tile.\nIgnores cloak. Lethal damage. Noisy.",
 	},
 }
 
