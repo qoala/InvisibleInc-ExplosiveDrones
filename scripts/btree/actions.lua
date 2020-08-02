32d227db06588ffc139caf0c7e9527264a57797a
Qoalabear <512290+qoala@users.noreply.github.com>
2020-08-01 22:05:23 -1000

    Don't crash if ReactToTarget loses its target with pulse drones

--- scripts/btree/actions.lua
+++ scripts/btree/actions.lua
@@ -18,6 +18,11 @@ function Actions.ReactToMissileTarget(sim, unit)
 		end
 	end
 	local target = unit:getBrain():getTarget()
+	if not target then
+		-- A pulse drone that had diagonal bonus vision turned, losing vision of the target.
+		-- Or something equally weird has gone wrong to "succeed" the old action without a target.
+		return simdefs.BSTATE_FAILED
+	end
 	local targetX,targetY = target:getLocation()
 	log:write( "DEBUG: TRACKING TARGET %d (%d,%d)", target:getID(), targetX, targetY )
 
