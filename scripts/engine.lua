9ca70bbaf3fcb046c0b48eaaf90966a599712111
Qoalabear <512290+qoala@users.noreply.github.com>
2020-08-01 22:05:22 -1000

    Explosive pulse drones prefer attack runs over generic hunt interests

--- scripts/engine.lua
+++ scripts/engine.lua
@@ -9,7 +9,12 @@ function simengine:scanCell( unit, cell, ignoreDisguise, scanGrenade )
 
 	local player = unit:getPlayerOwner()
 	-- Skip if the missile already has an interest. Should go there instead.
-	if unit:getTraits().pulseScanMissile and player and player:isNPC() and not unit:getBrain():getSenses():getCurrentInterest() then
+	if unit:getTraits().pulseScanMissile and player and player:isNPC() then
+		local currentInterest = unit:getBrain():getSenses():getCurrentInterest()
+		if currentInterest and simquery.isEnemyAgent( player, currentInterest.sourceUnit, true ) then
+			-- Don't interrupt an in-progress attack run
+			return
+		end
 		for i, cellUnit in ipairs( cell.units ) do
 			if simquery.isEnemyAgent( player, cellUnit, ignoreDisguise ) then
 				unit:getBrain():getSenses():addInterest( cell.x, cell.y, simdefs.SENSE_RADIO, simdefs.REASON_HUNTING, cellUnit, ignoreDisguise )
